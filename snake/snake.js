9d80e91a9efc6b61baa3f54bfce80f0937a0d3f4
= <=>
2013-02-14 11:18:35 -0800

    Can check if snakes dies. Can control snake using keydown events.

--- snake/snake.js
+++ snake/snake.js
@@ -23,7 +23,8 @@ function makeSnake() {
     snake: undefined,
 
     initialize: function() {
-      this.snake = [ [10, 10], [10, 11], [10, 12] ]; //left
+      this.snake = [ [10, 10], [10, 11], [10, 12], [10, 13], [10, 14] ]
+                     // [10, 15], [10, 16], [10, 17], [10, 18] ]; //left
       // this.snake = [ [10, 10], [10, 9], [10, 8] ]; // right
       // this.snake = [ [10, 10], [9, 10], [8, 10] ]; // down
       // this.snake = [ [10, 10], [11, 10], [12, 10] ]; // up
@@ -52,19 +53,20 @@ function makeSnake() {
       this.changePos(this.findDirection());
     },
 
+    // we need to refactor this very badly
     turn: function(direction) {
       current_direction = this.findDirection();
 
-      if ( current_direction == 'left' && direction == 'right') {
+      if ( current_direction == 'left' && direction == 'right' || current_direction == 'left' && direction == 'left') {
         // do nothing
         console.log("you're going left and trying to turn right");
-      } else if ( current_direction == 'right' && direction == 'left' ) {
+      } else if ( current_direction == 'right' && direction == 'left' || current_direction == 'right' && direction == 'right' ) {
         // do nothing
         console.log("you're going right and trying to turn left");
-      } else if ( current_direction == 'up' && direction == 'down' ) {
+      } else if ( current_direction == 'up' && direction == 'down' || current_direction == 'up' && direction == 'up' ) {
         // do nothing
         console.log("you're going up and trying to turn down");
-      } else if (current_direction == 'down' && direction == 'up' ) {
+      } else if (current_direction == 'down' && direction == 'up' || current_direction == 'down' && direction == 'down') {
         // do nothing
         console.log("you're going down and trying to turn up");
       } else {
@@ -90,9 +92,13 @@ function makeSnake() {
           this.snake.unshift([ head[0], head[1] + 1 ]);
           break;
       }
-      this.snake.pop();
-      console.log(this.snake);
+      if (this.eatsApple) {
+        // do nothing
+      } else {
+        this.snake.pop();
+      }
     }
+
   }
 }
 
@@ -101,6 +107,7 @@ function makeGame() {
 
     board: undefined,
     snake: undefined,
+    apple: undefined,
 
     initialize: function() {
       this.board = makeBoard();
@@ -108,40 +115,64 @@ function makeGame() {
 
       this.snake = makeSnake();
       this.snake.initialize();
+
+      this.apple = [ [Math.floor((Math.random()*20)), Math.floor((Math.random()*20))] ];
     },
 
-    isSnakeCoord: function(coordinate) {
-      row = coordinate[0];
-      col = coordinate[1];
-      snakeCoordBool = false;
+    containsCoord: function(coordinates, target_coord) {
+      coordBool = false;
 
-      this.snake.snake.forEach( function(coord) {
-
-        if (coord[0] == row && coord[1] == col) {
-          snakeCoordBool = true;
+      coordinates.forEach( function(coord) {
+        if (coord[0] == target_coord[0] && coord[1] == target_coord[1]) {
+          coordBool = true;
         }
       } );
-      return snakeCoordBool;
+      return coordBool;
     },
 
     makeBoardString: function() {
       var board_string = "";
+      console.log(this.apple);
 
       for(row = 0; row < this.board.board.length; row++) {
-
         for (col = 0; col < this.board.board[0].length; col++) {
 
-          if (this.isSnakeCoord([row, col])) {
+          if (this.containsCoord(this.snake.snake, [row, col])) {
             board_string += "[X]";
-          } else if (this.board.board[row][col] === '') {
+          } else if (this.containsCoord(this.apple, [row, col])) {
+            board_string += "[*]";
+          } else {
             board_string += "[ ]";
           }
         }
         board_string += "\n";
       }
       return board_string;
-    }
+    },
 
+    isDead: function() {
+      var headCoord = this.snake.snake[0];
+
+      if (headCoord[0] >= this.board.board.length || headCoord[0] < 0 ) {
+        console.log("DEAD DEAD DEAD: you hit the wall.");
+        return true;
+      } else if (headCoord[1] >= this.board.board[0].length || headCoord[1] < 0) {
+        console.log("DEAD DEAD DEAD: you hit the wall.");
+        return true;
+      } else if (this.containsCoord(this.snake.snake.slice(1), (headCoord))) {
+          console.log("DEAD DEAD DEAD: you ran into yourself.");
+          return true;
+      }
+    },
+
+    eatsApple: function() {
+      if (this.containsCoord(this.snake.snake, this.apple[0])) {
+        console.log("hit the apple");
+        this.apple.pop();
+        this.apple.push([Math.floor((Math.random()*20)), Math.floor((Math.random()*20))]);
+        return true;
+      }
+    }
 
   }
 }
--- snake/snake_ui.js
+++ snake/snake_ui.js
@@ -1,8 +1,25 @@
-STEP_TIME_MILLIS = 250;
-
+STEP_TIME_MILLIS = 500;
 
 $('html').keydown(function (event) {
   console.log("You pressed keycode: " + event.keyCode);
+  switch (event.keyCode) {
+    case 38:
+      game.snake.turn('up');
+      break;
+    case 40:
+      game.snake.turn('down');
+      break;
+    case 37:
+      game.snake.turn('left');
+      break;
+    case 39:
+      game.snake.turn('right');
+      break;
+  }
+  game.isDead();
+  clear();
+  println(game.makeBoardString());
+  window.setTimeout(STEP_TIME_MILLIS);
 } );
 
 
@@ -11,19 +28,27 @@ function println(string) {
   $('.output').append("\n");
 }
 
+function clear() {
+  $('.output').html("")
+}
 
 function runLoop() {
   // Schedule runLoop to be run again.
-  window.setTimeout(runStep(), STEP_TIME_MILLIS);
+  window.setTimeout(runStep, STEP_TIME_MILLIS);
 
 }
 
 function runStep() {
-  println(game.makeBoardString());
-  game.snake.step();
-  console.log(game.snake);
-  println(game.makeBoardString());
-
+  if (!game.isDead()) {
+    game.snake.step();
+    // game.eatsApple();
+    clear();
+    println(game.makeBoardString());
+    runLoop();
+  } else {
+    clear();
+    println(game.makeBoardString());
+  }
 }
 
 game = makeGame();
@@ -31,9 +56,6 @@ game.initialize();
 // console.log(game);
 // console.log(game.snake.snake);
 
-
-
-
-
 runLoop();
+
 
